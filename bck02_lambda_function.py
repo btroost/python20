9caecd0772f70a474eb318eae46f9f1ec9a5e918
btroost <59251634+btroost@users.noreply.github.com>
2025-02-26 21:43:26 +0100

    DynamoDB Connectie toegevoegd

--- bck02_lambda_function.py
+++ bck02_lambda_function.py
@@ -1,18 +1,45 @@
 import json
+import boto3
+#pip install boto3
 
+
+'''  gebruik dynamodb
+import boto3
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('your-table-name')
+    
+    response = table.get_item(Key={'your-primary-key': 'your-key-value'})
+    item = response.get('Item')
+    
+    return item
+
+'''
 def lambda_handler(event, context):
     #dump de input voor troubleshooting
     varq=json.dumps(event, indent=2)
+   
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('users')
 
+    mykeyvalue=event.get('arg1')
+    response = table.get_item(Key={'email': mykeyvalue})
+    ret1 = response.get('voornaam')
+   # return item
+
+    
     # Extract the two arguments from the event
     arg1 = event.get('arg1')
     arg2 = event.get('arg2')
-    ret1 =""
+    
+    ''' ret1 =""
     if arg1 == "eerste":
       ret1 = "tien"
     else:
       ret1 = "twintig"
-        
+    '''  
+
     # Perform some operation with the arguments
     result = f"Received arg1: {arg1}, arg2: {arg2}, return1: {ret1}"
         
--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,37 @@
 import json
+import boto3
+#*** script 20.a
+#pip install boto3
 
 def lambda_handler(event, context):
     #dump de input voor troubleshooting
     varq=json.dumps(event, indent=2)
+   
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('users')
 
+    mykeyvalue=event.get('arg1')
+    #mykeyvalue="abcd@xyz.com" # for testing
+    ret2="kokokok"
+    try:
+      response = table.get_item(Key={'email': mykeyvalue})
+      ret1 = response.get('Item')
+      if not ret1 == None:
+        try:
+          ret2 = ret1['voornaam']
+        except:
+          ret2 = "veld niet gevonden"
+      else:
+        ret2 = "record niet gevonden" 
+    except:
+      ret2 = "error in db request"
+    
     # Extract the two arguments from the event
     arg1 = event.get('arg1')
     arg2 = event.get('arg2')
-    ret1 =""
-    if arg1 == "eerste":
-      ret1 = "tien"
-    else:
-      ret1 = "twintig"
-        
+    
     # Perform some operation with the arguments
-    result = f"Received arg1: {arg1}, arg2: {arg2}, return1: {ret1}"
+    result = f"Received arg1: {arg1}, arg2: {arg2}, return1: {ret2}"
         
     # Return the result
     return {
